97166fff6751302a4ea03c723b1e2eb33b11d34d
DTAX-01 <yolandayudina@gmail.com>
2020-08-20 21:30:45 +0700

    FIRST

---
+++ formatRPRupiah.py
@@ -0,0 +1,9 @@
+import locale
+def rupiah_format(angka, with_prefix=False, desimal=2):
+    locale.setlocale(locale.LC_NUMERIC, 'IND')
+    rupiah = locale.format_string("%.*f", (desimal, angka), True)
+    if with_prefix:
+        return "Rp. {}".format(rupiah)
+    return rupiah
+
+print(rupiah_format(5000))
